3b333f107e560b12f946fded4ed0c217ef7d531d
Ryan Walklin <ryan@testtoast.com>
2014-05-24 14:43:57 +1200

    Initial kit

---
+++ CesiumKit/CesiumKit.h
@@ -0,0 +1,13 @@
+//
+//  CesiumKit.h
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 6/05/14.
+//  Copyright (c) 2014 Test Toast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CesiumKit : NSObject
+
+@end
---
+++ CesiumKitTests/CesiumKitTests.m
@@ -0,0 +1,34 @@
+//
+//  CesiumKitTests.m
+//  CesiumKitTests
+//
+//  Created by Ryan Walklin on 6/05/14.
+//  Copyright (c) 2014 Test Toast. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CesiumKitTests : XCTestCase
+
+@end
+
+@implementation CesiumKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
