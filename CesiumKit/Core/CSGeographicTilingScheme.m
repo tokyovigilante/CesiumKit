7d195b201f444f1c27e0713e9eadbe9bf81d89d9
Ryan Walklin <ryan@testtoast.com>
2014-05-30 19:48:17 +1200

    Add CSGeographicTilingScheme

---
+++ CesiumKit/Core/CSGeographicTilingScheme.m
@@ -0,0 +1,13 @@
+//
+//  CSGeographicTilingScheme.m
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 30/05/14.
+//  Copyright (c) 2014 Test Toast. All rights reserved.
+//
+
+#import "CSGeographicTilingScheme.h"
+
+@implementation CSGeographicTilingScheme
+
+@end
