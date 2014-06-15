2971f6f10d286b7a7d0ea48039243cbbb1c8edd3
Ryan Walklin <ryan@testtoast.com>
2014-06-15 17:32:36 +1200

    Matrix/Quarternion basics

---
+++ CesiumKit/Core/Matrix.swift
@@ -0,0 +1,39 @@
+//
+//  Matrix.swift
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 15/06/14.
+//  Copyright (c) 2014 Test Toast. All rights reserved.
+//
+
+import Foundation
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: Double[]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue: 0.0)
+    }
+    
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    func elementIndexForRow(row: Int, column: Int) -> Int {
+        assert(indexIsValidForRow(row, column: column), "Index out of range")
+        return (row * columns) + column
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
