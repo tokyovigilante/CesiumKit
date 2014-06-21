8e3591a15a177ccd1c328db2e48c65ca38f334be
Ryan Walklin <ryan@testtoast.com>
2014-06-21 16:38:43 +1200

    Add GeometryAttributes

--- CesiumKit/Core/Geometry.swift
+++ CesiumKit/Core/Geometry.swift
@@ -56,59 +56,96 @@
 *   boundingSphere : Cesium.BoundingSphere.fromVertices(positions)
 * });
 */
-
 struct Geometry {
 
-    var geometryAttributes: GeometryAttribute[]
+    /**
+    * Attributes, which make up the geometry's vertices.  Each property in this object corresponds to a
+    * {@link GeometryAttribute} containing the attribute's data.
+    * <p>
+    * Attributes are always stored non-interleaved in a Geometry.
+    * </p>
+    * <p>
+    * There are reserved attribute names with well-known semantics.  The following attributes
+    * are created by a Geometry (depending on the provided {@link VertexFormat}.
+    * <ul>
+    *    <li><code>position</code> - 3D vertex position.  64-bit floating-point (for precision).  3 components per attribute.  See {@link VertexFormat#position}.</li>
+    *    <li><code>normal</code> - Normal (normalized), commonly used for lighting.  32-bit floating-point.  3 components per attribute.  See {@link VertexFormat#normal}.</li>
+    *    <li><code>st</code> - 2D texture coordinate.  32-bit floating-point.  2 components per attribute.  See {@link VertexFormat#st}.</li>
+    *    <li><code>binormal</code> - Binormal (normalized), used for tangent-space effects like bump mapping.  32-bit floating-point.  3 components per attribute.  See {@link VertexFormat#binormal}.</li>
+    *    <li><code>tangent</code> - Tangent (normalized), used for tangent-space effects like bump mapping.  32-bit floating-point.  3 components per attribute.  See {@link VertexFormat#tangent}.</li>
+    * </ul>
+    * </p>
+    * <p>
+    * The following attribute names are generally not created by a Geometry, but are added
+    * to a Geometry by a {@link Primitive} or {@link GeometryPipeline} functions to prepare
+    * the geometry for rendering.
+    * <ul>
+    *    <li><code>position3DHigh</code> - High 32 bits for encoded 64-bit position computed with {@link GeometryPipeline.encodeAttribute}.  32-bit floating-point.  4 components per attribute.</li>
+    *    <li><code>position3DLow</code> - Low 32 bits for encoded 64-bit position computed with {@link GeometryPipeline.encodeAttribute}.  32-bit floating-point.  4 components per attribute.</li>
+    *    <li><code>position3DHigh</code> - High 32 bits for encoded 64-bit 2D (Columbus view) position computed with {@link GeometryPipeline.encodeAttribute}.  32-bit floating-point.  4 components per attribute.</li>
+    *    <li><code>position2DLow</code> - Low 32 bits for encoded 64-bit 2D (Columbus view) position computed with {@link GeometryPipeline.encodeAttribute}.  32-bit floating-point.  4 components per attribute.</li>
+    *    <li><code>color</code> - RGBA color (normalized) usually from {@link GeometryInstance#color}.  32-bit floating-point.  4 components per attribute.</li>
+    *    <li><code>pickColor</code> - RGBA color used for picking.  32-bit floating-point.  4 components per attribute.</li>
+    * </ul>
+    * </p>
+    *
+    * @type GeometryAttributes
+    *
+    * @default undefined
+    *
+    * @see GeometryAttribute
+    * @see VertexFormat
+    *
+    * @example
+    * geometry.attributes.position = new Cesium.GeometryAttribute({
+    *   componentDatatype : Cesium.ComponentDatatype.FLOAT,
+    *   componentsPerAttribute : 3,
+    *   values : new Float32Array()
+    * });
+    */
+    var geometryAttributes: GeometryAttributes
     
+    /**
+    * Optional index data that - along with {@link Geometry#primitiveType} -
+    * determines the primitives in the geometry.
+    *
+    * @type Array
+    *
+    * @default undefined
+    */
+    var indices: Array<Any>
+    
+    /**
+    * The type of primitives in the geometry.  This is most often {@link PrimitiveType.TRIANGLES},
+    * but can varying based on the specific geometry.
+    *
+    * @type PrimitiveType
+    *
+    * @default undefined
+    */
     var primitiveType: PrimitiveType
     
-    var indices
-    
+    /**
+    * An optional bounding sphere that fully encloses the geometry.  This is
+    * commonly used for culling.
+    *
+    * @type BoundingSphere
+    *
+    * @default undefined
+    */
     var BoundingSphere: BoundingSphere? = nil
+  
+    this.attributes = options.attributes;
     
-        var Geometry = function(options) {
-    options = defaultValue(options, defaultValue.EMPTY_OBJECT);
-    
-    //>>includeStart('debug', pragmas.debug);
-    if (!defined(options.attributes)) {
-    throw new DeveloperError('options.attributes is required.');
-}
-if (!defined(options.primitiveType)) {
-    throw new DeveloperError('options.primitiveType is required.');
-}
-//>>includeEnd('debug');
+    init (attributes: GeometryAttributes, indices: A)
 
-this.attributes = options.attributes;
 
-/**
-* Optional index data that - along with {@link Geometry#primitiveType} -
-* determines the primitives in the geometry.
-*
-* @type Array
-*
-* @default undefined
-*/
 this.indices = options.indices;
 
-/**
-* The type of primitives in the geometry.  This is most often {@link PrimitiveType.TRIANGLES},
-* but can varying based on the specific geometry.
-*
-* @type PrimitiveType
-*
-* @default undefined
-*/
+
 this.primitiveType = options.primitiveType;
 
-/**
-* An optional bounding sphere that fully encloses the geometry.  This is
-* commonly used for culling.
-*
-* @type BoundingSphere
-*
-* @default undefined
-*/
+
 this.boundingSphere = options.boundingSphere;
 };
 
---
+++ CesiumKit/Core/GeometryAttributes.swift
@@ -0,0 +1,92 @@
+//
+//  GeometryAttributes.swift
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 21/06/14.
+//  Copyright (c) 2014 Test Toast. All rights reserved.
+//
+
+/**
+* Attributes, which make up a geometry's vertices.  Each property in this object corresponds to a
+* {@link GeometryAttribute} containing the attribute's data.
+* <p>
+* Attributes are always stored non-interleaved in a Geometry.
+* </p>
+*
+* @alias GeometryAttributes
+* @constructor
+*/
+struct GeometryAttributes  {
+    
+    /**
+    * The 3D position attribute.
+    * <p>
+    * 64-bit floating-point (for precision).  3 components per attribute.
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var position: GeometryAttribute? = nil
+    
+    /**
+    * The normal attribute (normalized), which is commonly used for lighting.
+    * <p>
+    * 32-bit floating-point.  3 components per attribute.
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var normal: GeometryAttribute? = nil
+    
+    /**
+    * The 2D texture coordinate attribute.
+    * <p>
+    * 32-bit floating-point.  2 components per attribute
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var st: GeometryAttribute? = nil
+    
+    /**
+    * The binormal attribute (normalized), which is used for tangent-space effects like bump mapping.
+    * <p>
+    * 32-bit floating-point.  3 components per attribute.
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var binormal: GeometryAttribute? = nil
+    
+    /**
+    * The tangent attribute (normalized), which is used for tangent-space effects like bump mapping.
+    * <p>
+    * 32-bit floating-point.  3 components per attribute.
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var tangent: GeometryAttribute? = nil
+    
+    /**
+    * The color attribute.
+    * <p>
+    * 8-bit unsigned integer. 4 components per attribute.
+    * </p>
+    *
+    * @type GeometryAttribute
+    *
+    * @default undefined
+    */
+    var color: GeometryAttribute? = nil
+}
