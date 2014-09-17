85056ebf75e34d71d284825685a89c6cdd44a496
Ryan Walklin <ryan@testtoast.com>
2014-09-17 22:00:30 +1200

    Already did this 3 days ago...

--- CesiumKit/Renderer/VertexAttributes.swift
+++ CesiumKit/Renderer/VertexAttributes.swift
@@ -6,4 +6,41 @@
 //  Copyright (c) 2014 Test Toast. All rights reserved.
 //
 
-import Foundation
+struct VertexAttributes {
+    
+    var index: Int
+    
+    var enabled: Bool
+    
+    var vertexBuffer: Buffer
+    
+    var componentsPerAttribute: Int
+    
+    var componentDatatype: ComponentDatatype
+    
+    var normalize: Bool
+    
+    var offsetInBytes: Int
+    
+    var strideInBytes: Int
+    
+    init (
+        index: Int,
+        enabled: Bool = true,
+        vertexBuffer: Buffer,
+        componentsPerAttribute: Int,
+        componentDatatype: ComponentDatatype = .Float32,
+        normalize: Bool = false,
+        offsetInBytes: Int = 0,
+        strideInBytes: Int = 0) {
+            self.index = index
+            self.enabled = enabled
+            self.vertexBuffer = vertexBuffer
+            self.componentsPerAttribute = componentsPerAttribute
+            self.componentDatatype = componentDatatype
+            self.normalize = normalize
+            self.offsetInBytes = offsetInBytes
+            self.strideInBytes = strideInBytes
+    }
+    
+}
