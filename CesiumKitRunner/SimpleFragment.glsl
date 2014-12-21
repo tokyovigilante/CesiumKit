d7109eaa753357503034cb827b4225e8d7c3b4ba
Ryan Walklin <ryan@testtoast.com>
2014-12-21 22:11:01 +1300

    Delete debug shaders

--- CesiumKitRunner/SimpleFragment.glsl
+++
@@ -1,5 +0,0 @@
-varying lowp vec4 DestinationColor;
-
-void main(void) {
-	gl_FragColor = DestinationColor;
-}
--- CesiumKitRunner/SimpleVertex.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec4 Position;
-attribute vec4 SourceColor;
-
-varying vec4 DestinationColor;
-
-void main(void) {
-	DestinationColor = SourceColor;
-	gl_Position = Position;
-}
