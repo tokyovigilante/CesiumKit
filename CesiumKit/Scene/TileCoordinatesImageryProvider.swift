aaaa61d765adb5423903f6f424d4306461cc8ce9
Ryan Walklin <ryan@testtoast.com>
2015-02-21 21:19:04 +1100

    Add TileCoordinatesImageryProvider stub and re-disable terrain upsampling til texture bugs solved

---
+++ CesiumKit/Scene/TileCoordinatesImageryProvider.swift
@@ -0,0 +1,9 @@
+//
+//  TileCoordinatesImageryProvider.swift
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 21/02/15.
+//  Copyright (c) 2015 Test Toast. All rights reserved.
+//
+
+import Foundation
