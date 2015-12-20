a4f26e6550cb5873c2a1e65b4dbdca6369c8921d
Ryan Walklin <ryan@testtoast.com>
2015-12-21 08:28:59 +1100

    Add CesiumTerrainProvider stub

---
+++ CesiumTerrainProvider.swift
@@ -0,0 +1,13 @@
+//
+//  CesiumTerrainProvider.swift
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 21/12/2015.
+//  Copyright © 2015 Test Toast. All rights reserved.
+//
+
+import Foundation
+
+class CesiumTerrainProvider: TerrainProvider {
+    
+}
