2e546e4d8be97accdf73a662681c2c0277a5be9d
Ryan Walklin <ryan@testtoast.com>
2015-12-29 11:52:30 +1100

    Add Everest view example

--- CesiumKitRunner OSX/CesiumViewController.swift
+++ CesiumKitRunner OSX/CesiumViewController.swift
@@ -53,7 +53,11 @@ class CesiumViewController: NSViewController, MTKViewDelegate {
         //_globe.scene.camera.setView(position: Cartesian3.fromDegrees(longitude: 145.075, latitude: -37.892, height: 100), heading: 0, pitch: Math.toRadians(90), roll: 0)
 
         //Murrumbeena
-        //_globe.scene.camera.lookAt(Cartesian3.fromDegrees(longitude: 145.075, latitude: -37.892, height: 10), offsetCartesian: nil, offsetHPR: HeadingPitchRange(heading: 0.0, pitch: Math.toRadians(-90), range: 1000))
+        //_globe.scene.camera.lookAt(Cartesian3.fromDegrees(longitude: 145.075, latitude: -37.892, height: 1000), offsetCartesian: nil, offsetHPR: HeadingPitchRange(heading: 0.0, pitch: Math.toRadians(-90), range: 1000))
+        
+        // Everest
+        _globe.scene.camera.lookAt(Cartesian3.fromDegrees(longitude: 86.925278, latitude: 27.988056, height: 9000), offsetCartesian: nil, offsetHPR: HeadingPitchRange(heading: Math.toRadians(180.0), pitch: Math.toRadians(-90), range: 1000))
+        
         //Wellington
         //_globe.scene.camera.lookAt(Cartesian3.fromDegrees(longitude: 174.777222, latitude: -41.288889, height: 50000), target: Cartesian3.zero(), up: Cartesian3.unitZ())
         //_globe.scene.camera.viewRectangle(Rectangle.fromDegrees(west: 140.0, south: 20.0, east: 165.0, north: -90.0))
