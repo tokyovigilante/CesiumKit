e979144d8fd220e35e95ee13936aee8248a46816
Ryan Walklin <ryan@testtoast.com>
2016-01-24 15:18:15 +1300

    Add CartesianType protocol

---
+++ CesiumKit/Core/CartesianType.swift
@@ -0,0 +1,13 @@
+//
+//  CartesianType.swift
+//  CesiumKit
+//
+//  Created by Ryan Walklin on 24/01/2016.
+//  Copyright © 2016 Test Toast. All rights reserved.
+//
+
+import Foundation
+
+protocol CartesianType {
+    
+}
