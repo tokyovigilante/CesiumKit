c82d3cc70cb999e9ae0a8530b9cdc130485fd754
Ryan Walklin <ryan@testtoast.com>
2016-02-24 09:45:12 +0100

    Add Color material uniforms

--- CesiumKit/Scene/MaterialType.swift
+++ CesiumKit/Scene/MaterialType.swift
@@ -8,15 +8,21 @@
 
 import Foundation
 
-public protocol MaterialDescription {
+public protocol MaterialType {
+    var name: String { get }
     var fabric: FabricDescription { get }
+    var source: String? { get }
     var components: [String: String] { get }
     var translucent: (Material) -> Bool { get }
 }
 
-public struct ColorMaterialDescription: MaterialDescription {
+public struct ColorMaterialType: MaterialType {
+    
+    public let name = "Color"
     
     public var fabric: FabricDescription
+    
+    public var source: String? = nil
     
     public let components = [
         "diffuse": "color.rgb",
@@ -24,30 +30,47 @@ public struct ColorMaterialDescription: MaterialDescription {
     ]
     
     public let translucent = { (material: Material) in
-        return false//(material.fabric as ColorFabricDescription).color.alpha < 1.0
+        return (material.type.fabric as! ColorFabricDescription).color.alpha < 1.0
+    }
+    
+    public init (fabric: ColorFabricDescription = ColorFabricDescription(), source: String? = nil) {
+        self.fabric = fabric
+        self.source = source
     }
 }
 
-public protocol FabricDescription: UniformMap {}
+public protocol FabricDescription: UniformMap {
+    var uniformTypes: [String: UniformDataType] { get }
+}
 
 public struct ColorFabricDescription: FabricDescription {
     
-    var color: Color = Color(1.0, 1.0, 1.0, 1.0)
+    public var color: Color
     
-    public var uniforms: [String: UniformFunc] = [
-        "color": { (map: UniformMap, buffer: UnsafeMutablePointer<Void>) in
-            
-        }
-    ]
+    public let uniforms: [String: UniformFunc]
+    
+    public let uniformTypes: [String : UniformDataType]
+    
+    public init (color: Color = Color(fromRed: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)) {
+        
+        self.color = color
+        
+        uniforms = [
+            "color": { (map: UniformMap, buffer: UnsafeMutablePointer<Void>) in
+                let simd = (map as! ColorFabricDescription).color.floatRepresentation
+                memcpy(buffer, [simd], strideofValue(simd))
+            }
+        ]
+        
+        uniformTypes = [
+                "color": .FloatVec4
+        ]
+    }
 }
 
 public struct ImageFabricDescription: FabricDescription {
     public var uniforms: [String: UniformFunc]
-}
-
-public enum MaterialType {
     
-    case Color(ColorMaterialDescription)
+    public var uniformTypes: [String : UniformDataType]
     
-    //case Image(ImageMaterialDescription)
 }
